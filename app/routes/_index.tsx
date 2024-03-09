82106dd7ceb711fb8d3546998a9df6840d322a61
Sam Selikoff <sam.selikoff@gmail.com>
2024-03-09 14:21:07 -0500

    wip

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,25 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { codeToHtml } from "shiki";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+const inputCode = `
+function Page() {
+  let { user } = useLoaderData<typeof loader>()
+
+  return <p>Hello {user.name}!</p>
+}
+`;
+
+export async function loader() {
+  const html = await codeToHtml(inputCode, {
+    lang: "tsx",
+    theme: "dracula",
+  });
+
+  return { html };
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const { html } = useLoaderData<typeof loader>();
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
